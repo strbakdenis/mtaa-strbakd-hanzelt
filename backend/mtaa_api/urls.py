808057080ddc9f4a36af5e88ef95cb96652d087e
Tomáš Hanzel <hanzel.toma@gmail.com>
2021-03-25 13:36:23 +0100

    added registration and delete methods

--- backend/mtaa_api/urls.py
+++ backend/mtaa_api/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('test', views.test_user, name='test_user')
+    path('test', views.test_user, name='test_user'),
+    path('add', views.user_registration, name='user_registration'),
+    path('delete', views.user_delete, name='user_delete')
 ]
--- backend/mtaa_api/views.py
+++ backend/mtaa_api/views.py
@@ -2,14 +2,78 @@ from django.shortcuts import render
 from backend.mtaa_api.models import Users
 from django.http import JsonResponse
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import re
+import json
+import random, string
+from django.utils import timezone
 
-# Create your views here.
+    
+def generate_token():   
+    return(''.join(random.choices(string.ascii_letters + string.digits, k=16)))  
+
+def check_email(email): 
+  
+    regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+    
+    if(re.search(regex,email)): 
+        return True
+          
+    else: 
+        return False 
+
+def check_required_fields(request_body):
+    required_fields = ['password', 'email_address']
+
+    for field in required_fields:
+        if field not in request_body:
+            return False
+    return True
+
 
 def test_user(request):
-    user = Users(email_address = 'john.doe@gmail.com', password = 'password', token = 'asdfgh', registration_date='1900-10-10')
+    user = Users(email_address = 'john.doe@gmail.com', password = 'qwerty', token = 'asdfgh', registration_date='1900-10-10')
     user.save()
 
     return HttpResponse('asd')
 
+@csrf_exempt 
 def user_registration(request):
     
+    body = json.loads(request.body)
+    if check_required_fields(body) is False:
+        return HttpResponse(status=400)
+
+    if check_email(body['email_address']) is False:
+        return HttpResponse(status=400)
+    
+    try:
+        user_object = Users.objects.filter(email_address=body['email_address'])
+    except User.DoesNotExist:
+        user_object = None
+    if user_object:
+        return HttpResponse(status=409)
+    else:
+        random_token = generate_token()
+        user = Users(email_address = body['email_address'], password = body['password'], token = random_token, registration_date=timezone.now())
+        user.save()
+    
+    
+    return HttpResponse()
+
+@csrf_exempt 
+def user_delete(request):
+    token = request.GET.get('token', None)
+
+    if not token:
+        return HttpResponse(status=400) 
+
+    try:
+        user_object = Users.objects.filter(token=token)
+    except User.DoesNotExist:
+        user_object = None
+    if user_object:
+        Users.objects.filter(token=token).delete()
+        return HttpResponse(status=204)
+    else:
+       return HttpResponse(status=404)
